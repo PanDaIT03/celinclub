b1cc1056674687968c976677627de4e5e1108bec
Duong Dai <daiphucduongvinh203@gmail.com>
2024-08-16 22:24:23 +0700

    [STYLE] format code

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -2,6 +2,28 @@ import { Select, SelectProps } from 'antd';
 import { memo } from 'react';
 
 const SelectForm = (props: SelectProps) => {
+  const handleFilterOption = (input: any, option: any) => {
+    if (typeof option?.label === 'number')
+      return option.label.toString().includes(input);
+
+    return (option?.label ?? '')
+      .toString()
+      .toLowerCase()
+      .includes(input.toLowerCase());
+  };
+
+  const handleFilterSort = (optionA: any, optionB: any) => {
+    if (
+      typeof optionA?.label === 'number' ||
+      typeof optionB?.label === 'number'
+    )
+      return null;
+
+    return (optionA?.label ?? '')
+      .toLowerCase()
+      .localeCompare((optionB?.label ?? '').toLowerCase());
+  };
+
   return (
     <Select
       allowClear
@@ -9,26 +31,8 @@ const SelectForm = (props: SelectProps) => {
       size="large"
       optionFilterProp="children"
       className={`w-full ${props.className}`}
-      filterOption={(input: any, option: any) => {
-        if (typeof option?.label === 'number') {
-          return option.label.toString().includes(input);
-        }
-
-        return (option?.label ?? '')
-          .toString()
-          .toLowerCase()
-          .includes(input.toLowerCase());
-      }}
-      filterSort={(optionA: any, optionB: any) => {
-        if (
-          typeof optionA?.label === 'number' ||
-          typeof optionB?.label === 'number'
-        )
-          return null;
-        return (optionA?.label ?? '')
-          .toLowerCase()
-          .localeCompare((optionB?.label ?? '').toLowerCase());
-      }}
+      filterSort={handleFilterSort}
+      filterOption={handleFilterOption}
       {...props}
     />
   );
